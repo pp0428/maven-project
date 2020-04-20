de6279395d970a4f349ca2ab5de42680a702144e
jose mandujano <joseluis0428@gmail.com>
2020-04-20 17:46:34 -0500

    correcciones 2

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,10 +13,10 @@ public class Greeter {
   }
   
   /**
-   * @param semeone the name
+   * @param semeone the name of person
    * @return greeting string
    ***/
-  public final String  greet(String  final someone) {
+  public final String  greet(final String   someone) {
     return String.format("Hello, %s!", someone);
   }
 }
